e395f8d7a8ee3343d543807d25320f445088b4a8
hmohammedak <hmohammedak@gmail.com>
2023-03-13 11:59:52 -0700

    13 March 2023

---
+++ FirstCode/src/FirstCode.java
@@ -0,0 +1,13 @@
+public class FirstCode {
+    public static void main(String[] args){
+
+    }
+    public static boolean sleepIn(boolean weekday, boolean vacation){
+        if (weekday == false || vacation == true) {
+            return true;
+        }
+        else{
+               return false;
+            }
+        }
+    }
---
+++ Sum/src/Sum.java
@@ -0,0 +1,13 @@
+   public class Sum{
+           System.out.println(Sum(10, 12));
+          }System.out.println(Sum(5, 5));
+
+
+
+  public static int Sum(int a, int b) {
+      if (a == b){
+      return 2*(a+b);
+} else{
+     return (a+b);
+}
+}
