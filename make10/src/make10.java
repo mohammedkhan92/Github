cdb604cb9d32f5aaad681a94945b3dea4146e4ad
hmohammedak <hmohammedak@gmail.com>
2023-03-13 17:40:56 -0700

    13 march test

---
+++ make10/src/make10.java
@@ -0,0 +1,19 @@
+class makes10 {
+
+    public static void main(String[] args) {
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(true);
+    }
+
+    public boolean makes10(int a, int b) {
+        if ((a == 10) || (b == 10) || ((a + b) == 10)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+
