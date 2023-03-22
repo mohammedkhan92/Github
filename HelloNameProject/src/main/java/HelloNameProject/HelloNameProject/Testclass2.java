58fc7bd4ea890fd14e73a238128db4cccc2b3eb9
hmohammedak <hmohammedak@gmail.com>
2023-03-21 18:18:36 -0700

    20 march 23

---
+++ HelloNameProject/src/main/java/HelloNameProject/HelloNameProject/Testclass2.java
@@ -0,0 +1,14 @@
+package HelloNameProject.HelloNameProject;
+
+
+public class Testclass2 {
+
+//write a method to print numbers from 1 to 5
+
+    public static void main(String[] args) {
+        System.out.println("hn");
+
+    }
+}
+
+
