0defaab299fafb535fac83bece2f898e74e66cda
hmohammedak <hmohammedak@gmail.com>
2023-03-26 20:51:29 -0700

    26th March 2023

---
+++ Sum/src/Product1.java
@@ -0,0 +1,9 @@
+public class Product1 {
+
+    public static void main(String[] args){
+        Product1(18, 12);
+    }
+    public static void Product1(int a, int b){
+        System.out.println(a*b);
+    }
+}
