b6a083b39dbbad18c13a39c4be955190bb701e2f
hmohammedak <hmohammedak@gmail.com>
2023-04-04 18:33:54 -0700

    4th April 2023

---
+++ Spring-boot-tutorial/src/test/java/com/dailycodebuffer/Springboot/tutorial/tutorial/controller/DepartmentControllerTest.java
@@ -0,0 +1,18 @@
+package com.dailycodebuffer.Springboot.tutorial.tutorial.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(DepartmentController)
+class DepartmentControllerTest {
+
+    @Test
+    void saveDepartment() {
+    }
+
+    @Test
+    void fetchDepartmentById() {
+    }
+}
