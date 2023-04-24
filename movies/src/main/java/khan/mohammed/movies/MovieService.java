a5fe6c822c7efd3f73f1b1430ad0476fdd45e66c
hmohammedak <hmohammedak@gmail.com>
2023-04-23 22:59:41 -0700

    04/23/2023

---
+++ movies/src/main/java/khan/mohammed/movies/MovieService.java
@@ -0,0 +1,8 @@
+package khan.mohammed.movies;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+    public allMovies()
+}
---
+++ movies/src/test/java/khan/mohammed/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package khan.mohammed.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
