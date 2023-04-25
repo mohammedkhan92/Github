f36ecddc67932989364bca4a6fe05305ee2bcc2d
hmohammedak <hmohammedak@gmail.com>
2023-04-25 07:56:58 -0700

    04/25/2023

--- movies/src/main/java/khan/mohammed/movies/ReviewService.java
+++ movies/src/main/java/khan/mohammed/movies/ReviewService.java
@@ -14,15 +14,19 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public Review createReview(String reviewBody,String imdbId) {
-        Review review = new Review(reviewBody);
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review =reviewRepository.insert(new Review(reviewBody));
 
         reviewRepository.insert(review);
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review));
-    }
+        first();
 
+        return review;
+    }
 }
+
+
 
